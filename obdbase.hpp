11a6e382c591a999299d50d072f8408f9f3c10b2
hayouni <hayouniaymen@yahoo.fr>
2021-01-09 22:31:21 +0100

    Add database connexion

--- obdbase.hpp
+++ obdbase.hpp
@@ -1,11 +1,25 @@
 #ifndef OBDBASE_HPP
 #define OBDBASE_HPP
 
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QFile>
+#include <QSqlError>
+#include <QSqlQueryModel>
+#include "log.hpp"
 
 class OBDBase
 {
-public:
+private:
     OBDBase();
+    static OBDBase *instance;
+    void init();
+    QSqlDatabase db;
+public:
+    static OBDBase* getInstance();
+    static void ResetInstance();
+    QSqlDatabase getDBInstance();
+    ~OBDBase();
 };
 
 #endif // OBDBASE_HPP
